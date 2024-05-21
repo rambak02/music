79355f9054c8b2e6c9b1342903cfc0f3b46d74c2
rambak02 <rambak02@mail.ru>
2024-05-21 20:38:59 +0700

    Bar

--- sky-pro-musicv2/src/app/page.tsx
+++ sky-pro-musicv2/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import clsx from "clsx";
 import { NavBar } from "@/components/NavBar/NavBar";
@@ -6,6 +5,7 @@ import { Playlist } from "@/components/Playlist/Playlist";
 import { Filter } from "@/components/Filter/Filter";
 import { Search } from "@/components/Search/Search";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
+import { Bar } from "@/components/Bar/Bar";
 
 export default function Home() {
   return (
@@ -21,107 +21,7 @@ export default function Home() {
           </div>
           <Sidebar />
         </div>
-        <div className={styles.bar}>
-          <div className={styles.bar__content}>
-             <div className={styles.bar__playerProgress}></div>
-            <div className={styles.bar__playerBlock}>
-              <div className={clsx(styles.bar__player, styles.player)}>
-                <div className={styles.player__controls}>
-                  <div className={styles.player__btnPrev}>
-                    <svg className={styles.player__btnPrevSvg}>
-                      <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
-                    </svg>
-                  </div>
-                  <div className={clsx(styles.player__btnPlay, styles._btn)}>
-                    <svg className={styles.player__btnPlaySvg}>
-                      <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
-                    </svg>
-                  </div>
-                  <div className={styles.player__btnNext}>
-                    <svg className={styles.player__btnNextSvg}>
-                      <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
-                    </svg>
-                  </div>
-                  <div
-                    className={clsx(styles.player__btnRepeat, styles._btnIcon)}
-                  >
-                    <svg className={styles.player__btnRepeatSvg}>
-                      <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
-                    </svg>
-                  </div>
-                  <div
-                    className={clsx(styles.player__btnShuffle, styles._btnIcon)}
-                  >
-                    <svg className={styles.player__btnShuffleSvg}>
-                      <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
-                    </svg>
-                  </div>
-                </div>
-
-                <div
-                  className={clsx(styles.player__trackPlay, styles.trackPlay)}
-                >
-                  <div className={styles.trackPlay__contain}>
-                    <div className={styles.trackPlay__image}>
-                      <svg className={styles.trackPlay__svg}>
-                        <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
-                      </svg>
-                    </div>
-                    <div className={styles.trackPlay__author}>
-                      <a
-                        className={styles.trackPlay__authorLink}
-                        href="http://"
-                      >
-                        Ты та...
-                      </a>
-                    </div>
-                    <div className={styles.trackPlay__album}>
-                      <a className={styles.trackPlay__albumLink} href="http://">
-                        Баста
-                      </a>
-                    </div>
-                  </div>
-
-                  <div className={styles.trackPlay__likeDis}>
-                    <div
-                      className={clsx(styles.trackPlay__like, styles._btnIcon)}
-                    >
-                      <svg className={styles.trackPlay__likeSvg}>
-                        <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
-                      </svg>
-                    </div>
-                    <div
-                      className={clsx(
-                        styles.trackPlay__dislike,
-                        styles._btnIcon
-                      )}
-                    >
-                      <svg className={styles.trackPlay__dislikeSvg}>
-                        <use xlinkHref="img/icon/sprite.svg#icon-dislike"></use>
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className={clsx(styles.bar__volumeBlock, styles.volume)}>
-                <div className={styles.volume__content}>
-                  <div className={styles.volume__image}>
-                    <svg className={styles.volume__svg}>
-                      <use xlinkHref="img/icon/sprite.svg#icon-volume"></use>
-                    </svg>
-                  </div>
-                  <div className={clsx(styles.volume__progress, styles._btn)}>
-                    <input
-                      className={clsx(styles.volume__progressLine, styles._btn)}
-                      type="range"
-                      name="range"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+       <Bar />
         <footer className={styles.footer}></footer>
       </div>
     </div>
