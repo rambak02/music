0842dc7bc9211e324ccf6fa3cf87cf2af19091fa
rambak02 <rambak02@mail.ru>
2024-08-20 17:06:14 +0300

    поиск

--- sky-pro-musicv2/src/components/Search/Search.tsx
+++ sky-pro-musicv2/src/components/Search/Search.tsx
@@ -1,18 +1,24 @@
 import clsx from "clsx";
-import styles from "./Search.module.css"
+import styles from "./Search.module.css";
+import { useAppDispatch } from "@/hooks/store";
+import { useState } from "react";
+import { setFilter } from "@/store/features/playlistSlice";
 
 export const Search = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <div className={clsx(styles.centerblock__search, styles.search)}>
-    <svg className={styles.search__svg}>
-      <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-    </svg>
-    <input
-      className={styles.search__text}
-      type="search"
-      placeholder="Поиск"
-      name="search"
-    />
-  </div>
+      <svg className={styles.search__svg}>
+        <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
+      </svg>
+      <input
+        className={styles.search__text}
+        type="search"
+        placeholder="Поиск"
+        name="search"
+        onChange={(e) => dispatch(setFilter({ searchString: e.target.value }))}
+      />
+    </div>
   );
 };
