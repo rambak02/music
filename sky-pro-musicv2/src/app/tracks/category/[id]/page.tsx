4e6dfa571b5d345326253939c24cd0f1a0c1add6
rambak02 <rambak02@mail.ru>
2024-09-02 07:48:21 +0700

    исправлена верстка

--- sky-pro-musicv2/src/app/tracks/category/[id]/page.tsx
+++ sky-pro-musicv2/src/app/tracks/category/[id]/page.tsx
@@ -1,43 +1,84 @@
-'use client'
+"use client";
 import { getCategory } from "@/api/categoryApi";
 import styles from "../../layout.module.css";
 import { Playlist } from "@/components/Playlist/Playlist";
-import { useEffect, useRef } from "react";
-import { setCurrentPlaylist, setCurrentTrack, setInitialPlaylist} from "@/store/features/playlistSlice";
+import { useEffect, useRef, useState } from "react";
+import {
+  setCurrentPlaylist,
+  setCurrentTrack,
+  setInitialPlaylist,
+} from "@/store/features/playlistSlice";
 import { useAppDispatch, useAppSelector } from "@/hooks/store";
+import { TrackType } from "@/types/type";
+import { getTracks } from "@/api/api";
 
 type CategoryProps = {
   params: {
     id: string;
-  }
+  };
 };
+// useEffect(() => {
+//   getPlaylistTracks(params.id)
+//     .then((tracksData) => {
+//       setTracks(tracksData);
+//       dispatch(setInitialTracks({ initialTracks: tracksData }));
+//     })
+//     .catch((error: any) => {
+//       throw new Error(error.message);
+//     });
+// }, [dispatch, params.id]);
 
 function Category({ params }: CategoryProps) {
   const dispatch = useAppDispatch();
-  const allTracks = useAppSelector((state) => state.playlist.initialPlaylist)
-  const name = useRef();
+  const allTracks = useAppSelector((state) => state.playlist.initialPlaylist);
 
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const res = await getTracks();
+        dispatch(setInitialPlaylist(res));
+      } catch (error) {
+        console.error("Failed to fetch tracks:", error);
+      }
+    };
+    fetchTracks();
+  }, [dispatch]);
+  
   useEffect(() => {
     try {
       getCategory(params.id).then((res) => {
-        console.log(res)
-        name.current = res.name;
         const items = res.items;
-        const tracks = items.map((item: number) => allTracks.filter((track) => track._id === item)).flat();
+        const tracks = items
+          .map((item: number) =>
+            allTracks.filter((track) => track._id === item)
+          )
+          .flat();
         dispatch(setCurrentPlaylist(tracks));
-      })
-    } catch (error) {
+      });
+    } catch (error) {}
+  }, [allTracks, dispatch, params.id]);
 
-    }
-  }, [dispatch, allTracks, params.id]);
-
+  let title = "";
+  switch (params.id) {
+    case "2":
+      title = "Плейлист дня";
+      break;
+    case "3":
+      title = "100 танцевальных хитов";
+      break;
+    case "4":
+      title = "Инди-заряд";
+      break;
+    default:
+      break;
+  }
 
   return (
     <>
-      <h2 className={styles.centerblock__h2}>{name.current}</h2>
+      <h2 className={styles.centerblock__h2}>{title}</h2>
       <Playlist />
     </>
   );
-};
+}
 
 export default Category;
